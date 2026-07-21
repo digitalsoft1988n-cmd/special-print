fd50e2a6f49b5042cd1720168abcaab5e01354e2
digitalsoft1988n-cmd <digitalsoft1988n@gmail.com>
2026-07-21 21:19:28 +0530

    feat: add double border style

--- special_print/styles/__init__.py
+++ special_print/styles/__init__.py
@@ -4,9 +4,11 @@ Border styles for Special Print.
 
 from .rounded import BorderStyle, ROUNDED
 from .ascii import ASCII
+from .double import DOUBLE
 
 __all__ = [
     "BorderStyle",
     "ASCII",
     "ROUNDED",
+    "DOUBLE",
 ]
